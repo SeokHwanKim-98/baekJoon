3e82f4f6e6d8b1c8e85c819b698e330ff8eb539d
SeokHwanKim-98 <133570351+SeokHwanKim-98@users.noreply.github.com>
2023-12-20 15:31:58 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 64 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,12 @@
+namespace test3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputYear = Console.ReadLine();
+            int year = int.Parse(inputYear);
+            Console.WriteLine(year - 543);
+        }
+    }
+}
