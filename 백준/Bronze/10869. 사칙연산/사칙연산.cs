d8262fde4733b7963a8c077ce8a1c353db8195ce
SeokHwanKim-98 <133570351+SeokHwanKim-98@users.noreply.github.com>
2023-12-20 15:16:28 +0900

    [Bronze V] Title: 사칙연산, Time: 64 ms, Memory: 5520 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,18 @@
+namespace Test2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+            string[] sNum = num.Split();
+            int a = int.Parse(sNum[0]);
+            int b = int.Parse(sNum[1]);
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+        }
+    }
+}
