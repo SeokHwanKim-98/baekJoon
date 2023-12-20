1052c6768f56de8e6b368332a483c2cf50c38ee3
SeokHwanKim-98 <133570351+SeokHwanKim-98@users.noreply.github.com>
2023-12-20 15:25:19 +0900

    [Bronze V] Title: ??!, Time: 60 ms, Memory: 5380 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,11 @@
+namespace test3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine(name+"??!");
+        }
+    }
+}
