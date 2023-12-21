689f641d0bafd34f95539be23cfcad9cb9837741
SeokHwanKim-98 <133570351+SeokHwanKim-98@users.noreply.github.com>
2023-12-21 18:02:09 +0900

    [Bronze V] Title: 고양이, Time: 60 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,13 @@
+namespace test5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( \')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+        }
+    }
+}
