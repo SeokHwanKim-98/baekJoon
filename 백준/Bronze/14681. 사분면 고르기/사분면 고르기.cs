95b6ee7ebb4dafb30c6c5c9e13216ba5c298848f
SeokHwanKim-98 <133570351+SeokHwanKim-98@users.noreply.github.com>
2023-12-22 18:44:30 +0900

    [Bronze V] Title: 사분면 고르기, Time: 64 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cs
@@ -0,0 +1,16 @@
+namespace _12_22._23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if (x > 0 && y > 0) { Console.WriteLine(1); }
+            else if (x < 0 && y > 0) { Console.WriteLine(2); }
+            else if (x < 0 && y < 0) { Console.WriteLine(3); }
+            else if (x > 0 && y < 0) { Console.WriteLine(4); }
+        }
+    }
+}
