ae7066e52da7f0c029f9f2fd128a11f4ba64538c
SeokHwanKim-98 <133570351+SeokHwanKim-98@users.noreply.github.com>
2024-01-10 18:11:52 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 60 ms, Memory: 5444 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cs
@@ -0,0 +1,12 @@
+namespace _0110_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine(); //문자열 입력받음
+            
+            Console.WriteLine(str.Length);
+        }
+    }
+}
