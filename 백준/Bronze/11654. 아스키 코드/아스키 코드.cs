1c7debe2b92d77a084bedce59161a7cfd118ca17
SeokHwanKim-98 <133570351+SeokHwanKim-98@users.noreply.github.com>
2024-01-12 18:08:54 +0900

    [Bronze V] Title: 아스키 코드, Time: 64 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,13 @@
+namespace _0112_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char firstLe = input[0];
+            int ascii = Convert.ToInt32(firstLe);
+            Console.WriteLine(ascii);
+        }
+    }
+}
