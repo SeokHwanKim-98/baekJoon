3757d744e9b1c29de9fb0470cea7b99ccc91af94
SeokHwanKim-98 <133570351+SeokHwanKim-98@users.noreply.github.com>
2024-01-15 20:01:12 +0900

    [Bronze V] Title: 새싹, Time: 56 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,15 @@
+namespace _0115_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("         ,r'\"7");
+            Console.WriteLine("r`-_   ,'  ,/");
+            Console.WriteLine(" \\. \". L_r'");
+            Console.WriteLine("   `~\\/");
+            Console.WriteLine("      |");
+            Console.WriteLine("      |");
+        }
+    }
+}
